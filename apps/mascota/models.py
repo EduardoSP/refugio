0beac06fcf5c24ef05bd660fcc96d5e29d83f15b
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-02-04 21:26:49 -0500

    cambiando bien

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -5,8 +5,6 @@ from apps.adopcion.models import Persona
 class Vacuna(models.Model):
     nombre          = models.CharField(max_length=50)
 
-
-
 class Mascota(models.Model):
     folio           = models.CharField(max_length=10, primary_key=True)
     nombre          = models.CharField(max_length=50)
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 
 #prueba tutorial
-
-hola = 5
