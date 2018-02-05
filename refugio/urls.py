c349769333ff4e20f9ed166195cbe2fe7759945c
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-02-05 12:04:04 -0500

    video 8 urls

--- refugio/urls.py
+++ refugio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^mascota/', include('apps.mascota.urls', namespace="mascota")),
+    url(r'^adopcion/', include('apps.adopcion.urls', namespace="adopcion")),
 ]
