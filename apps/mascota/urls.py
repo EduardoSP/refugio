415617cc7576bb3fafb7e4580ade6bb8fef0eb3a
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-03-17 18:56:46 -0500

    listar y eliminar

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from apps.mascota.views import index, mascota_view, mascota_list
+from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
 
 app_name='mascota'
 urlpatterns = [
     url(r'^$', index, name='index'), #^(significa donde inicia la cadena) y #$ (significa donde termina la cadena)
     url(r'^nuevo$', mascota_view, name='mascota_crear'),
     url(r'^listar$', mascota_list, name='mascota_listar'),
+    url(r'^editar/(?P<id_mascota>\d+)$', mascota_edit, name='mascota_editar'),
+    url(r'^eliminar/(?P<id_mascota>\d+)$', mascota_delete, name='mascota_eliminar'),
 ]
