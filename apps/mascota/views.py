d9ef438899820b8e499b3fd1d515e5dca8b236c5
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-03-17 16:46:46 -0500

    video 14 listar

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from apps.mascota.forms import MascotaForm
+from apps.mascota.models import Mascota
 # Create your views here.
 
 def index(request):
@@ -16,3 +17,11 @@ def mascota_view(request):
     else:
         form = MascotaForm()
     return render(request, 'mascota/mascota_form.html', {'form':form})
+
+def mascota_list(request):
+    #vista basada en funciones
+    #creo queryset
+    mascota     = Mascota.objects.all()
+    #envio el queryset en el contexto
+    contexto    = {'mascotas':mascota} 
+    return render(request, 'mascota/mascota_list.html', contexto)
