41b72af1043cac3790687b621b4655ddd42a8847
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-04-02 19:37:48 -0500

    inicio crud dos formularios video 17

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from apps.adopcion.views import index_adopcion
+from apps.adopcion.views import index_adopcion, SolicitudList
 
 app_name='adopcion'
 urlpatterns = [
-    url(r'^index$', index_adopcion, name='home')
+    url(r'^index$', index_adopcion, name='home'),
+    url(r'^solicitud/listar$', SolicitudList.as_view(), name='solicitud_listar')
 ]
--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, CreateView
+
+from apps.adopcion.models import Persona, Solicitud
+from apps.adopcion.forms import PersonaForm, SolicitudForm
 # Create your views here.
 
 def index_adopcion(request):
     return HttpResponse("Soy la pagina principal de la app adopcion")
+
+class SolicitudList(ListView):
+    model           = Solicitud
+    template_name   = 'adopcion/solicitud_list.html'
+
+class SolicitudCreate(CreateView):
+    model               = Solicitud
+    template_name       = 'adopcion/solicitud_form.html'
+    form_class          =  SolicitudForm
+    second_form_class   = PersonaForm
+    #success_url         = reverse_lazy('')
+
 
