3fe3c7e3bcabcaf0a05ebbacaf1519b9e1e60a94
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-04-02 23:15:20 -0500

    fin video 17

--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 
 from apps.adopcion.models import Persona, Solicitud
@@ -18,6 +19,35 @@ class SolicitudCreate(CreateView):
     template_name       = 'adopcion/solicitud_form.html'
     form_class          =  SolicitudForm
     second_form_class   = PersonaForm
-    #success_url         = reverse_lazy('')
+    success_url         = reverse_lazy('adopcion:solicitud_listar')
+
+    def get_context_data(self, **kwargs):
+        #sobreescribo el metodo para agregar los dos formularios en el contexto
+        context =super(SolicitudCreate, self).get_context_data(**kwargs)
+        if 'form' not in context:
+            #si el form no esta en el contexto se lo asignamos
+            context['form'] = self.form_class(self.request.GET)
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(self.request.GET)
+        return context
+        
+    #para que los dos formularios se guarden y se cree la relacion
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        #recojo la informacion de los formularios
+        form = self.form_class(request.POST)
+        form2 = self.second_form_class(request.POST)
+        if form.is_valid() and form2.is_valid():
+            solicitud           = form.save(commit=False)  #No guarde hasta que se guarde el segundo formulario
+            solicitud.persona   = form2.save()
+            solicitud.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            #si no es valido devuelvo el contexto
+            return self.render_to_response(self.get_context_data(form=form, form2=form2))
+
+
+        
+        
 
 
